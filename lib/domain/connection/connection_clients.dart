0bc76366680b734044ab0ede815810d42a0411eb
sahya <athena.starlight.searcher+github@gmail.com>
2026-03-22 20:13:54 +0900

    fix(connection): auto wire client events for reconnect

--- lib/domain/connection/connection_clients.dart
+++ lib/domain/connection/connection_clients.dart
@@ -1,16 +1,54 @@
+enum SessionWsEventType {
+  disconnected,
+  broadcastEnded,
+}
+
+class SessionWsEvent {
+  const SessionWsEvent(this.type);
+
+  final SessionWsEventType type;
+}
+
 abstract class SessionWsClient {
+  Stream<SessionWsEvent> get events;
+
   Future<SessionEndpoints> connectAndResolveEndpoints();
 
   Future<void> disconnect();
 }
 
+enum NdgrEventType {
+  disconnected,
+  stalled,
+}
+
+class NdgrEvent {
+  const NdgrEvent(this.type);
+
+  final NdgrEventType type;
+}
+
 abstract class NdgrClient {
+  Stream<NdgrEvent> get events;
+
   Future<void> connect(Uri viewApiUri);
 
   Future<void> disconnect();
 }
 
+enum LegacyCommentEventType {
+  disconnected,
+}
+
+class LegacyCommentEvent {
+  const LegacyCommentEvent(this.type);
+
+  final LegacyCommentEventType type;
+}
+
 abstract class LegacyCommentClient {
+  Stream<LegacyCommentEvent> get events;
+
   Future<void> connect(Uri wsUrl);
 
   Future<void> disconnect();
