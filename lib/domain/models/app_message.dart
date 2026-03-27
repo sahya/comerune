665ad2ab04e6e25b906b309b84694f956534ae63
sahya <athena.starlight.searcher+github@gmail.com>
2026-03-28 04:03:41 +0900

    fix(issue-2): address pr30 should-fix review items

--- lib/domain/models/app_message.dart
+++ lib/domain/models/app_message.dart
@@ -22,4 +22,23 @@ class AppMessage {
   final String content;
   final AppMessageType type;
   final Object? raw;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+
+    return other is AppMessage &&
+        other.id == id &&
+        other.timestamp == timestamp &&
+        other.userId == userId &&
+        other.content == content &&
+        other.type == type;
+  }
+
+  @override
+  int get hashCode {
+    return Object.hash(id, timestamp, userId, content, type);
+  }
 }
